57367f5f7ad9b5926ea40e3cb8a9a9432fda8402
Matt Spencer <spencermatt93@gmail.com>
2026-02-09 11:05:15 +0000

    Add first test

---
+++ tests/test_list_of_books.py
@@ -0,0 +1,8 @@
+from lib.list_of_books import list_of_books
+
+def test_list_of_books_returns_list_with_one_book():
+    actual = list_of_books(['ABC'])
+    expected = ['ABC']
+
+    assert actual == expected
+    
